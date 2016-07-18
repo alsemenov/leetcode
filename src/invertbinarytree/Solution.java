d26b534807bb4bc7616fb1a2ebaa54ca1a10d1f0
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-07-18 11:13:56 +0600

    my solution for 226. Invert Binary Tree

---
+++ src/invertbinarytree/Solution.java
@@ -0,0 +1,33 @@
+package invertbinarytree;
+/**
+ * <p>226. Invert Binary Tree
+ * <p><a href="https://leetcode.com/problems/invert-binary-tree/">https://leetcode.com/problems/invert-binary-tree/</a>
+ * <p>Invert a binary tree.
+<pre>
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+</pre>
+ *
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root==null){
+            return root;
+        }
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
