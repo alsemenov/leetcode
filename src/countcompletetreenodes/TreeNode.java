370589ad93937ea4051340e243662426676853ac
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-07-19 10:28:20 +0600

    my solution for 222. Count Complete Tree Nodes

---
+++ src/countcompletetreenodes/TreeNode.java
@@ -0,0 +1,25 @@
+package countcompletetreenodes;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+
+	public TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
