789f3e1cc73f374ff5dbf0391125656343f1c02f
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-07-21 16:27:54 +0600

    my solution for 203. Remove Linked List Elements

---
+++ src/deletenodeinalinkedlist/Solution.java
@@ -0,0 +1,17 @@
+package deletenodeinalinkedlist;
+/**
+ * <p>237. Delete Node in a Linked List
+ * <p><a href="https://leetcode.com/problems/delete-node-in-a-linked-list/">https://leetcode.com/problems/delete-node-in-a-linked-list/</a>
+ * <p>Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * <p>Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ *
+ */
+public class Solution {
+	/**
+	 * copy value from next element and delete the next element
+	 */
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
