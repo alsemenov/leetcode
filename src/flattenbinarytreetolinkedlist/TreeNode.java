635405dbdf782ea125650c80dfa055bf7ab779e1
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-08-18 17:41:46 +0700

    my solution for 114. Flatten Binary Tree to Linked List

---
+++ src/flattenbinarytreetolinkedlist/TreeNode.java
@@ -0,0 +1,19 @@
+package flattenbinarytreetolinkedlist;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
