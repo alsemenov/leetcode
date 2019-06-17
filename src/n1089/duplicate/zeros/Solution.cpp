274d78284273acc7e082a430e4594233e04fe8a8
Aleksei Semenov <semals@yandex-team.ru>
2019-06-17 12:45:37 +0700

    14. Longest Common Prefix

---
+++ src/n1089/duplicate/zeros/Solution.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    /**
+    Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+    Note that elements beyond the length of the original array are not written.
+
+    Do the above modifications to the input array in place, do not return anything from your function.
+
+
+    Example 1:
+
+    Input: [1,0,2,3,0,4,5,0]
+    Output: null
+    Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+    Example 2:
+
+    Input: [1,2,3]
+    Output: null
+    Explanation: After calling your function, the input array is modified to: [1,2,3]
+     */
+    void duplicateZeros(vector<int>& arr) {
+        int zeroCount = count(arr.begin(), arr.end(), 0);
+        int s = arr.size() - 1;
+        int d = arr.size() + zeroCount - 1;
+        while (s>=0) {
+            if (d < arr.size()) {
+                arr[d] = arr[s];
+            }
+            if (arr[s] == 0) {
+                d--;
+                if (d < arr.size()) {
+                    arr[d] = arr[s];
+                }
+            }
+
+            s--;
+            d--;
+        }
+
+    }
+};
